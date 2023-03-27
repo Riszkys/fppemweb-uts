2b2208eb73a1c2349d4b6b46a36900f4bca74f5a
kiki <126378515+Riszkys@users.noreply.github.com>
2023-03-27 12:21:26 +0700

    Update scriptLOGINREGISTER.js

--- scriptLOGINREGISTER.js
+++ scriptLOGINREGISTER.js
@@ -20,3 +20,32 @@ btnPopup.addEventListener("click", () => {
 iconClose.addEventListener("click", () => {
   wrapper.classList.remove("active-popup");
 });
+
+document.querySelectorAll(".btn-KP").forEach((item) => {
+  item.addEventListener("click", (e) => {
+    let parent = e.target.parentNode.parentNode;
+
+    let gambar = parent.querySelector(".card-img-top").src;
+    let judul = parent.querySelector(".card-title").innerHTML;
+    let deskripsi = parent.querySelector(".deskripsi")
+      ? parent.querySelector(".deskripsi").innerHTML
+      : "<i>tidak ada informasi yang tersedia</i>";
+    let tombolmodal = document.querySelector(".btnmodal");
+    tombolmodal.click();
+
+    document.querySelector(".modaltittle").innerHTML = judul;
+    let image = document.createElement("img");
+    image.src = gambar;
+    image.classList.add("w-100");
+    document.querySelector(".modalimage").innerHTML = "";
+    document.querySelector(".modalimage").appendChild(image);
+
+    const nohp = "6285942966129";
+    let pesan = `https://api.whatsapp.com/send?phone=${nohp}&text= halo saya mau beli tiket ini ${gambar}`;
+
+    document.querySelector(".btnbeli").href = pesan;
+  });
+});
+$("#myModal").on("shown.bs.modal", function () {
+  $("#myInput").trigger("focus");
+});
